e021f0073d50d0cdece54dd85f5af20ef77c54c5
Hameed Abdulrahaman <hameedabdulrahamann@gmail.comgit config --global user.name Hameed>
2020-01-16 21:37:50 +0100

    chat file upload

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-var ws = require('socket.io').listen(5000);
+var socketio = require('socket.io');
+var SocketIOFileUpload = require("socketio-file-upload");
+var express = require('express');
 var mysql = require('mysql');
+
+var app = express()
+    .use(SocketIOFileUpload.router)
+    .use(express.static(__dirname))
+    .listen(5000);
+var ws = socketio.listen(app);
 
 var conn = mysql.createConnection({
     host     : 'localhost',
@@ -15,6 +23,7 @@ var isInitChat = false;
 var socketCount = 0;
 
 ws.sockets.on('connection', function(socket){
+
     socketCount++
     ws.sockets.emit('socket count', socketCount)
  
@@ -22,13 +31,26 @@ ws.sockets.on('connection', function(socket){
         socketCount--
         ws.sockets.emit('socket count', socketCount)
     })
+
+    var uploader = new SocketIOFileUpload();
+    uploader.dir = __dirname + '/uploads';
+
+    uploader.listen(socket);
+
+    uploader.on('saved', function(event){
+        console.log(event.file);
+    });
+
+    uploader.on('error', function(event){
+        console.log("Error from uploader", event);
+    })
  
     socket.on('new chat', function(data){
        
         chats.push(data)
         ws.sockets.emit('new chat', data)
-        console.log(data.chat);
-        conn.query('INSERT INTO chats (chat) VALUES (?)', data.chat)
+        console.log(data);
+        conn.query('INSERT INTO chats (chat, image) VALUES (?, ?)', [data.chat, data.image]);
     })
     
     if (! isInitChat) {
